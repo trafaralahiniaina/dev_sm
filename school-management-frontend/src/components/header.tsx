4e0f1f1637d797a4412e910628cb08af656d9af4
trafaralahiniaina <tahina.rafaralahiniaina@gmail.com>
2024-06-08 13:19:29 +0300

    Mise en place du navbar, footer

--- school-management-frontend/src/components/header.tsx
+++ school-management-frontend/src/components/header.tsx
@@ -4,7 +4,35 @@ import Image from "next/image";
 
 const Header: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-gray-800 text-white">
+    <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias ex aut eveniet. Illo perspiciatis fugiat a earum soluta dignissimos officiis adipisci eius distinctio hic, esse voluptatem odit vel cupiditate, facilis obcaecati necessitatibus incidunt nulla accusamus officia iste debitis. Sequi, eos? </div>
+  );
+};
+
+export default Header;
+
+
+{/* 
+  <div className="mx-auto max-w-7xl px-6 py-4 lg:px-8">
+        <div className="relative flex h-12 md:h-20 items-center justify-between">
+          <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
+
+            {/* LOGO *
+
+            <div className="flex flex-shrink-0 items-center">
+              <img
+                className="block h-12 w-40 lg:hidden"
+                src={'/assets/logo/logo.svg'}
+                alt="dsign-logo" />
+              <img
+                className="hidden h-full w-full lg:block"
+                src={'/assets/logo/logo.svg'}
+                alt="dsign-logo" />
+            </div>
+          </div>
+        </div>
+      </div>
+  
+  <header className="fixed top-0 left-0 right-0 z-50 bg-zinc-800 text-white">
       <div className="container mx-auto flex justify-between items-center py-4 px-8">
         <div className="flex items-center">
           <div className="w-10 h-10 bg-white rounded-full mr-4"></div>
@@ -35,8 +63,4 @@ const Header: React.FC = () => {
           </ul>
         </nav>
       </div>
-    </header>
-  );
-};
-
-export default Header;
+    </header> */}
--- school-management-frontend/src/pages/home.tsx
+++ school-management-frontend/src/pages/home.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import Header from '@/components/header';
 import Body from '@/components/main_home';
-import Footer from '@/components/footer';
 
 const Index: React.FC = () => {
   return (
     <main >
-      <Header/>
       <Body/>
-      <Footer/>
     </main>
   );
 };
